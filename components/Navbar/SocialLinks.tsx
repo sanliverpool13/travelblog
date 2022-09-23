8177ed5676cf4a0104a921d2bdaf5e33bfcfca54
sanliverpool13 <kairosh.sanjar@gmail.com>
2022-09-23 13:57:05 -0400

    Add social link urls

--- components/Navbar/SocialLinks.tsx
+++ components/Navbar/SocialLinks.tsx
@@ -10,13 +10,25 @@ interface Props {
 const SocialLinks: React.FC<Props> = ({ mobile }) => {
   return (
     <SocialLinksContainer mobile={mobile}>
-      <SocialLink href="" mobile={mobile}>
+      <SocialLink
+        href="https://www.instagram.com/thejeletsetters/"
+        target="_blank"
+        mobile={mobile}
+      >
         <FaInstagram />
       </SocialLink>
-      <SocialLink href="" mobile={mobile}>
+      <SocialLink
+        href="https://www.youtube.com/channel/UCn_pog_Wkk8CF-g9uDWVwXQ"
+        target="_blank"
+        mobile={mobile}
+      >
         <FaYoutube />
       </SocialLink>
-      <SocialLink href="" mobile={mobile}>
+      <SocialLink
+        href="https://www.tiktok.com/@thejeletsetters"
+        target="_blank"
+        mobile={mobile}
+      >
         <FaTiktok />
       </SocialLink>
     </SocialLinksContainer>
