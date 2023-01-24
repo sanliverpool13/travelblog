664336c2ca6e2ec24179d3d47d7f14d1550e0dd3
sanliverpool13 <kairosh.sanjar@gmail.com>
2023-01-24 17:37:11 -0500

    Updated head title

--- components/Footer/style.ts
+++ components/Footer/style.ts
@@ -10,6 +10,7 @@ export const FooterContainer = styled.div`
   padding: 0.5rem 0;
   font-size: 0.75rem;
   background-color: var(--bg-clr-footer);
+
   color: #f7f9fb;
   margin-bottom: 0;
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,16 @@ import type { AppProps } from "next/app";
 import GlobalStyle from "../global_styles";
 import Layout from "../components/layout";
 import { RecoilRoot } from "recoil";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <GlobalStyle />
+
+      <Head>
+        <title>Beyond Us Two</title>
+      </Head>
 
       <Layout>
         <Component {...pageProps} />
