dcb35a5d551f4a95bccb843a539d737bd2840c85
sanliverpool13 <kairosh.sanjar@gmail.com>
2023-03-19 19:58:43 -0400

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import GlobalStyle from "../global_styles";
 import Layout from "../components/layout";
 import { RecoilRoot } from "recoil";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -12,6 +13,22 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Beyond Us Two</title>
       </Head>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-JSW1J95JPC"
+      />
+
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'G-JSW1J95JPC');`,
+        }}
+      />
 
       <Layout>
         <Component {...pageProps} />
