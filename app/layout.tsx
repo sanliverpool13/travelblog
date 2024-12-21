5ae6d41eab24e1fdf7b19f642222a54a28c87b9d
sanjarjelet13 <126490244+sanjarjelet13@users.noreply.github.com>
2024-12-21 10:48:02 -0500

    add logo to metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
 import Head from "next/head";
+import type { Metadata } from "next";
 import PageLayout from "../components/pagelayout";
 import { Analytics } from "@vercel/analytics/react";
 import "./global.css";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Jelet Adventures",
+  description:
+    "A blog to share our adventures, whether in travel, food or anything else.",
 };
 
 export default function RootLayout({ children }) {
